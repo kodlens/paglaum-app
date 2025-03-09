b391842d3fdc810602b61e80cacdb84003713bb5
kodlens <etiennewayne@gmail.com>
2025-03-09 18:53:50 +0800

    adding application loan form

--- database/migrations/2025_02_06_133249_create_loan_subtypes_table.php
+++ database/migrations/2025_02_06_133249_create_loan_subtypes_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('loan_subtypes', function (Blueprint $table) {
             $table->id();
+            
+            $table->unsignedBigInteger('loan_type_id');
+            $table->foreign('loan_type_id')->references('id')
+                ->on('loan_types')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->string('loan_subtype')->nullable();
+            $table->double('term_month')->default(0);
+            $table->double('percent')->default(0);
+            $table->tinyInteger('active')->default(1);
             $table->timestamps();
         });
     }
