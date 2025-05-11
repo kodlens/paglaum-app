2016ae8ea24c7117e73d1c653d00967420f66a49
kodlens <etiennewayne@gmail.com>
2025-05-11 17:07:05 +0800

    scaffold online deposit payment

--- resources/js/Pages/Member/MySavings/DepositOnlineIndex.tsx
+++ resources/js/Pages/Member/MySavings/DepositOnlineIndex.tsx
@@ -1,25 +1,36 @@
+import { Head } from "@inertiajs/react";
 import { Button, Form, Input, InputNumber } from "antd";
 import { WalletCards } from "lucide-react";
 
 export default function DepositOnlineIndex() {
 
-    return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-100">
+    const handleOnlinePay = () => {
 
-            <div className="bg-white rounded-md p-6">
-                <div className="font-bold">How much would you like to deposit?</div>
-                <div>
-                    <Form.Item>
-                        <InputNumber />
-                    </Form.Item>
-                </div>
-                <div>
-                    <Button type="primary"
-                        icon={<WalletCards size={16}/>}>
-                        Pay
-                    </Button>
+    }
+
+    
+    return (
+        <>
+            <Head title="Savings Online Payment" />
+            <div className="min-h-screen flex justify-center items-center bg-gray-100">
+
+                <div className="bg-white rounded-md p-6 mx-2 shadow-md">
+                    <div className="font-bold mb-4">How much would you like to deposit?</div>
+                    <div>
+                        <Form.Item>
+                            <InputNumber type="number" className="w-full" />
+                        </Form.Item>
+                    </div>
+                    <div>
+                        <Button type="primary"
+                            onClick={handleOnlinePay}
+                            icon={<WalletCards size={16}/>}>
+                            Pay
+                        </Button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
+        
     )
 }
